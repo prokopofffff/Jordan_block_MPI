20509e87f20751ebe6ad565735dbb7756e5b82f7
Иван Кухаренко <jonya_ko@Ivans-MacBook-Pro.local>
2025-04-26 19:38:33 +0300

    fixed code

--- jordan.cpp
+++ jordan.cpp
@@ -12,10 +12,11 @@ int Jordan(double *A, double *B, double *X, double *C, double *block, double *do
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     double t_start = MPI_Wtime();
     double norm = A[0];
-    rank++;
 
-    std::cout << "[MPI] Process " << rank << " Jordan() start\n";
-    std::cout << "[MPI] Size" << size << "\n";
+    if (rank == 0) {
+        std::cout << "[MPI] Process " << rank << " Jordan() start\n";
+        std::cout << "[MPI] Size " << size << "\n";
+    }
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
@@ -27,158 +28,82 @@ int Jordan(double *A, double *B, double *X, double *C, double *block, double *do
     int l = n % m;
     int h = (l == 0 ? k : k + 1);
     int red = 0;
-    int bpt = k / size;
+    // int bpt = k / size;
 
     for (int r = 0; r < h; r++) {
-        int kp = (h - r - 1) / size;
-        int lp = (h - r - 1) % size;
-        int is_last_k = (r == k);
-        int block_size = is_last_k ? l : m;
+        int block_size = (r == k) ? l : m;
 
-        get_block(A, block, n, m, r, r);
-        std::cout << "block before inverse\n";
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                std::cout << A[i * n + j] << " ";
-
-            }
-            std::cout << std::endl;
-
-        }
-        int status = inverse(block, C, block_size, norm);
-        std::cout << "block after inverse\n";
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                std::cout << C[i * n + j] << " ";
-
-            }
-            std::cout << std::endl;
-
-        }
-        MPIReduceSum(&status, &status, 1);
-        if (status == -1) return -1;
-        set_block(A, block, n, m, r, r);
-
-        for (int x = 0; x < kp; x++) {
-            int s = r + x * size + rank;
-            int is_last_s = (s == k);
-            int block_size_s = is_last_s ? l : m;
-
-            get_block(A, block, n, m, r, s);
-            multiply(C, block, dop_mat, block_size, block_size, block_size, block_size_s);
-            set_block(A, dop_mat, n, m, r, s);
+        if (rank == r % size) {
+            get_block(A, block, n, m, r, r);
+            int status = inverse(block, C, block_size, norm);
+            MPI_Bcast(&status, 1, MPI_INT, r % size, MPI_COMM_WORLD);
+            if (status == -1) return -1;
+            set_block(A, block, n, m, r, r);
+            MPI_Bcast(block, block_size * block_size, MPI_DOUBLE, r % size, MPI_COMM_WORLD);
+            MPI_Bcast(C, block_size * block_size, MPI_DOUBLE, r % size, MPI_COMM_WORLD);
+        } else {
+            MPI_Bcast(&red, 1, MPI_INT, r % size, MPI_COMM_WORLD); 
+            if (red == -1) return -1;
+            get_block(A, block, n, m, r, r); 
+            MPI_Bcast(block, block_size * block_size, MPI_DOUBLE, r % size, MPI_COMM_WORLD);
+            MPI_Bcast(C, block_size * block_size, MPI_DOUBLE, r % size, MPI_COMM_WORLD);
+            set_block(A, block, n, m, r, r);
         }
 
-        std::cout << "block after multiplication\n";
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                std::cout << A[i * n + j] << " ";
-
-            }
-            std::cout << std::endl;
-
-        }
-
-        if (rank <= lp) {
-            int s = r + kp * size + rank;
-            int is_last_s = (s == k);
-            int block_size_s = is_last_s ? l : m;
-
-            get_block(A, block, n, m, r, s);
-            multiply(C, block, dop_mat, block_size, block_size, block_size, block_size_s);
-            set_block(A, dop_mat, n, m, r, s);
-        }
-
-        std::cout << "block after dop multiplication\n";
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                std::cout << A[i * n + j] << " ";
-
-            }
-            std::cout << std::endl;
-
-        }
-
-        if (rank == size) {
+        if (rank == r % size) {
             get_vector(B, X, n, m, r);
             multiply(C, X, dop_mat, block_size, block_size, block_size, 1);
             set_vector(B, dop_mat, n, m, r);
+            MPI_Bcast(dop_mat, block_size, MPI_DOUBLE, r % size, MPI_COMM_WORLD);
+        } else {
+            MPI_Bcast(dop_mat, block_size, MPI_DOUBLE, r % size, MPI_COMM_WORLD);
+            set_vector(B, dop_mat, n, m, r);
         }
 
-        std::cout << "block after vector multiplication\n";
-        for(int i = 0; i < n; i++){
-            std::cout << B[i] << " ";
-            std::cout << std::endl;
+        for (int s = r + 1; s < h; s++) {
+            int block_size_s = (s == k) ? l : m;
+            if (rank == s % size) {
+                get_block(A, block, n, m, r, s);
+                multiply(C, block, dop_mat, block_size, block_size, block_size, block_size_s);
+                set_block(A, dop_mat, n, m, r, s);
+                MPI_Bcast(dop_mat, block_size * block_size_s, MPI_DOUBLE, s % size, MPI_COMM_WORLD);
+            } else {
+                MPI_Bcast(dop_mat, block_size * block_size_s, MPI_DOUBLE, s % size, MPI_COMM_WORLD);
+                set_block(A, dop_mat, n, m, r, s);
+            }
         }
-        
+
+        for (int i = 0; i < h; i++) {
+            if (i == r) continue;
+            int block_size_i = (i == k) ? l : m;
+            if (rank == i % size) {
+                get_block(A, C, n, m, i, r);
+                for (int j = r + 1; j < h; j++) {
+                    int block_size_j = (j == k) ? l : m;
+                    get_block(A, block, n, m, r, j);
+                    multiply(C, block, dop_mat, block_size_i, block_size, block_size, block_size_j);
+                    get_block(A, block, n, m, i, j);
+                    subtract(block, dop_mat, block_size_i, block_size_j);
+                    set_block(A, block, n, m, i, j);
+                    MPI_Bcast(block, block_size_i * block_size_j, MPI_DOUBLE, i % size, MPI_COMM_WORLD);
+                }
+                get_vector(B, X, n, m, r);
+                multiply(C, X, dop_mat, block_size_i, block_size, block_size, 1);
+                get_vector(B, X, n, m, i);
+                subtract(X, dop_mat, block_size_i, 1);
+                set_vector(B, X, n, m, i);
+                MPI_Bcast(X, block_size_i, MPI_DOUBLE, i % size, MPI_COMM_WORLD);
+            } else {
+                for (int j = r + 1; j < h; j++) {
+                    int block_size_j = (j == k) ? l : m;
+                    MPI_Bcast(block, block_size_i * block_size_j, MPI_DOUBLE, i % size, MPI_COMM_WORLD);
+                    set_block(A, block, n, m, i, j);
+                }
+                MPI_Bcast(X, block_size_i, MPI_DOUBLE, i % size, MPI_COMM_WORLD);
+                set_vector(B, X, n, m, i);
+            }
+        }
         MPI_Barrier(MPI_COMM_WORLD);
-        
-        for(int j = 0; j <= bpt; j++){
-            int i = j * size + rank - 1;
-            MPIReduceSum(&red, &red, 1);
-            if(i >= r) i++;
-            if(i >= h) break;
-            int block_size_rows = (i == k) ? l : m;
-
-            get_block(A, C, n, m, i, r);
-
-            for(int c = r + 1; c < h; c++){
-                int block_size_cols = (c == k) ? l : m;
-
-                get_block(A, block, n, m, r, c);
-                multiply(C, block, dop_mat, block_size_rows, m, m, block_size_cols);
-                get_block(A, block, n, m, i, c);
-                subtract(block, dop_mat, block_size_rows, block_size_cols);
-                set_block(A, block, n, m, i, c);
-            }
-
-            std::cout << "block after sub\n";
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < n; j++){
-                    std::cout << A[i * n + j] << " ";
-                }
-                std::cout << std::endl;
-            }
-            std::cout << j << "!!!!!!!!!!!!!\n";
-            get_vector(B, X, n, m, r);
-            std::cout << "vector before multiply\n";
-            for(int i = 0; i < n; i++){
-                std::cout << X[i] << " ";
-            }
-            std::cout << std::endl;
-            std::cout << "block CCCCCCCCCCC\n";
-            for(int i = 0; i < n; i++){
-                for(int j = 0; j < m; j++){
-                    std::cout << C[i * n + j] << " ";
-                }
-                std::cout << std::endl;
-            }
-            multiply(C, X, dop_mat, block_size_rows, block_size, block_size, 1);
-            std::cout << "vector after multiply\n";
-            for(int i = 0; i < n; i++){
-                std::cout << dop_mat[i] << " ";
-            }
-            std::cout << std::endl;
-            get_vector(B, X, n, m, i);
-            std::cout << "vector before sub\n";
-            for(int i = 0; i < n; i++){
-                std::cout << X[i] << " ";
-            }
-            std::cout << std::endl;
-            subtract(X, dop_mat, block_size_rows, 1);
-            std::cout << "vector after sub\n";
-            for(int i = 0; i < n; i++){
-                std::cout << X[i] << " ";
-            }
-            std::cout << std::endl;
-            set_vector(B, X, n, m, i);
-            std::cout << "vector after set\n";
-            for(int i = 0; i < n; i++){
-                std::cout << B[i] << " ";
-            }
-            std::cout << std::endl;
-        }
-        MPIReduceSum(&red, &red, 1);
     }
 
     if (rank == 0) {
@@ -186,8 +111,8 @@ int Jordan(double *A, double *B, double *X, double *C, double *block, double *do
             X[i] = B[i];
         }
     }
-
+    MPI_Bcast(X, n, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     double t_end = MPI_Wtime();
-    printf("[MPI] Process %d Jordan() time: %.6f seconds\n", rank, t_end - t_start);
+    if (rank == 0) printf("[MPI] Process %d Jordan() time: %.6f seconds\n", rank, t_end - t_start);
     return 1;
 }
